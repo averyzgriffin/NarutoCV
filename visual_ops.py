8d8b31ba0a2e69cefe742dacfcb55447af89733b
Avery Griffin <averyzanegriffin@gmail.com>
2020-01-29 18:35:40 -0500

    Cleaned up the global and instance variables for the character icon class

--- visual_ops.py
+++ visual_ops.py
@@ -1,7 +1,8 @@
 import pygame
 import time
-import global_variables as gb
+import global_variables as glob_var
 import jutsu_signs
+from game_manager import GameManager
 
 
 black = (0,0,0)
@@ -28,14 +29,14 @@ def create_textObject(text, font):
 
 
 def get_jutsu_selected_visual(jutsu, win):
-    visual_cue = VisualCue(msg=str(jutsu.get_jutsu_signs()), w=gb.display_width, h=(gb.display_height*0.0625),
-                          text_color=black, typ=[], seq=[], x=0, y=gb.display_height * .75, win=win)
+    visual_cue = VisualCue(msg=str(jutsu.get_jutsu_signs()), w=glob_var.display_width, h=(glob_var.display_height*0.0625),
+                          text_color=black, typ=[], seq=[], x=0, y=glob_var.display_height * .75, win=win)
 
     text_ = "You have selected: " + str(jutsu.jutsu_icon_name)
-    font = pygame.font.Font("freesansbold.ttf", int(9.259259259259259e-05 * gb.display_area * .5))
+    font = pygame.font.Font("freesansbold.ttf", int(9.259259259259259e-05 * glob_var.display_area * .5))
 
     textsurf, textRect = create_textObject(text_, font)
-    textRect.center = (gb.display_width/2, (gb.display_height*.25))
+    textRect.center = (glob_var.display_width/2, (glob_var.display_height*.25))
     win.blit(textsurf, textRect)
 
     pygame.display.update()
@@ -49,7 +50,7 @@ def get_jutsu_selected_visual(jutsu, win):
 # ------------------------------------------------------------------------------
 class Button:
 
-    font = pygame.font.Font("freesansbold.ttf", int(3.7037037037037037e-05 * gb.display_area * .5))
+    font = pygame.font.Font("freesansbold.ttf", int(3.7037037037037037e-05 * glob_var.display_area * .5))
     is_clicked = False
     clickable = False
     class_clickable = False
@@ -109,57 +110,47 @@ class Button:
 
 class CharacterIcon:
 
-    p1_x = gb.display_width * .025
-    p2_x = gb.display_width * (7/8)
-    top_y = gb.display_height / 5
-    middle_y = gb.display_height * (2 / 5)
-    bottom_y = gb.display_height * (3 / 5)
-    folder = 'character_icons/'
-    icon_size = (int(gb.display_width * .1), int(gb.display_height * .15))
+    _folder = 'character_icons/'
+    icon_size = (int(glob_var.display_width * .1), int(glob_var.display_height * .15))
 
-    class_clickable = False
-    class_isclicked = False
+    char_highlighted = False  # rename this
+    queued_to_be_attack = None  # rename this
 
-    char_highlighted = False
-    queued_to_be_attack = None
-    class_turn = True
-
-    def __init__(self, icon_name, player_num, icon_num, win):
+    def __init__(self, icon_name, player_num, icon_num):
         self.icon_name = icon_name
         self.player_num = player_num
         self.icon_num = icon_num
-        self.win = win
-        self.clickable = False
+
+        self.img = self.resize_image()  # I have no idea how this is being called
         self.health = 100
-        self.img = self.resize_image()
 
     def get_x(self):
         if self.player_num == 1:
-            x_location = self.p1_x
+            x_location = glob_var.display_width * .025
         elif self.player_num == 2:
-            x_location = self.p2_x
+            x_location = glob_var.display_width * (7/8)
         else:
             return "invalid PLAYER number provided to Class Icon get_x method"
         return x_location
 
     def get_y(self):
         if self.icon_num == 1:
-            y_location = self.top_y
+            y_location = glob_var.display_height / 5
         elif self.icon_num == 2:
-            y_location = self.middle_y
+            y_location = glob_var.display_height * (2 / 5)
         elif self.icon_num == 3:
-            y_location = self.bottom_y
+            y_location = glob_var.display_height * (3 / 5)
         else:
             return "invalid ICON number provided to Class Icon get_y method"
         return y_location
 
     def get_image_from_string(self):
         try:
-            path = self.folder + self.icon_name + ".jpg"
+            path = self._folder + self.icon_name + ".jpg"
             img = pygame.image.load(path)
         except Exception as e:
             try:
-                path = self.folder + self.icon_name + ".png"
+                path = self._folder + self.icon_name + ".png"
                 img = pygame.image.load(path)
             except Exception as e:
                 return f"Couldn't find jpg nor png for icon {self.icon_name}."
@@ -170,14 +161,14 @@ class CharacterIcon:
         resized = pygame.transform.scale(img, self.icon_size)
         return resized
 
-    def click_status(self):
+    def click_status(self):  # This can be cleaned up
         mouse = pygame.mouse.get_pos()
         x, y = self.get_x(), self.get_y()
         size = self.icon_size[0]
-
-        if (x + size) > mouse[0] > x and (y + size) > mouse[1] > y: self.clickable = True
-        else: self.clickable = False
-        return self.clickable
+        if (x + size) > mouse[0] > x and (y + size) > mouse[1] > y:
+            return True
+        else:
+            return False
 
     def get_position(self):
         x, y = self.get_x(), self.get_y()
@@ -197,7 +188,7 @@ class CharacterIcon:
 
         # Click logic
         if Jutsu_Icon.class_isclicked:
-            if CharacterIcon.class_turn and self.player_num == 2 or not CharacterIcon.class_turn and self.player_num == 1:
+            if GameManager.player_turn and self.player_num == 2 or not GameManager.player_turn and self.player_num == 1:
                 img = self.highlight_image(img)
 
                 if self.click_status():
@@ -207,7 +198,7 @@ class CharacterIcon:
                         print('Character Clicked On')
                         CharacterIcon.queued_to_be_attack = self
         # Blit image
-        self.win.blit(img, (self.get_x(), self.get_y()))
+        glob_var.win.blit(img, (self.get_x(), self.get_y()))
         self.display_bar()
 
     def check_health(self):
@@ -218,37 +209,43 @@ class CharacterIcon:
         input('death' + str(self.icon_name))
 
     def display_bar(self):
-        bar_x, bar_y = self.get_x(), self.get_y() + self.icon_size[1] + (gb.display_height * 0.00625)
+        bar_x, bar_y = self.get_x(), self.get_y() + self.icon_size[1] + (glob_var.display_height * 0.00625)
         bar_width = int(self.icon_size[0] * (self.health / 100))
 
         bar = pygame.Surface((bar_width, 10), pygame.SRCALPHA)
         bar.fill((255, 0, 0, 255))  # includes the alpha value
 
         bar_message = f"Health:  {self.health}"
-        font = pygame.font.Font("freesansbold.ttf", int(1.8518518518518518e-05 * gb.display_area * .5))
+        font = pygame.font.Font("freesansbold.ttf", int(1.8518518518518518e-05 * glob_var.display_area * .5))
         textsurf, textRect = create_textObject(bar_message, font)
-        textRect.center = (bar_x + (self.icon_size[0] / 2), bar_y + (gb.display_height * 0.0225))
+        textRect.center = (bar_x + (self.icon_size[0] / 2), bar_y + (glob_var.display_height * 0.0225))
 
-        self.win.blit(bar, (bar_x, bar_y))
-        self.win.blit(textsurf, textRect)
+        glob_var.win.blit(bar, (bar_x, bar_y))
+        glob_var.win.blit(textsurf, textRect)
+
 
 
 class Jutsu_Icon(CharacterIcon):
 
-    icon_size = (int(gb.display_width * 0.06666666666666667), int(gb.display_height * 0.1))
-    xOffset_from_character_icon = gb.display_width // 12
-    queued_for_attack = None
-    folder = 'jutsu_icons/'
+    _folder = 'jutsu_icons/'
+    icon_size = (int(glob_var.display_width * 0.06666666666666667), int(glob_var.display_height * 0.1))
 
-    def __init__(self, icon_name, player_num, icon_num, parent_icon, win):
-        super().__init__(icon_name, player_num, icon_num, win)
+    class_clickable = False
+    class_isclicked = False
+
+    xOffset_from_character_icon = glob_var.display_width // 12
+    queued_for_attack = None
+
+
+    def __init__(self, icon_name, player_num, icon_num, parent_icon):
+        super().__init__(icon_name, player_num, icon_num)
         self.parent_icon = parent_icon
 
 
     def get_x(self):
         x_p = self.parent_icon.get_x()
         if self.player_num == 1:
-            x_p += (gb.display_width * .025)  # was 30
+            x_p += (glob_var.display_width * .025)  # was 30
             x = x_p + (self.xOffset_from_character_icon * self.icon_num)
         elif self.player_num == 2:
             x = x_p - (self.xOffset_from_character_icon * self.icon_num)  # mirror effect
@@ -258,7 +255,7 @@ class Jutsu_Icon(CharacterIcon):
 
     def get_y(self):
         y_p = self.parent_icon.get_y()
-        y = y_p + (gb.display_height * 0.025)
+        y = y_p + (glob_var.display_height * 0.025)
         return y
 
     def display_image(self):
@@ -292,22 +289,22 @@ class Jutsu_Icon(CharacterIcon):
                     Jutsu_Icon.class_isclicked = False
                     Jutsu_Icon.queued_for_attack = None
 
-        self.win.blit(img, (x, y))
+        glob_var.win.blit(img, (x, y))
         self.display_name()
 
     def display_name(self):
         msg1 = self.icon_name
         msg2 = f"Damage: {self.get_damage()}"
-        font = pygame.font.Font("freesansbold.ttf", int(2.2222222222222223e-05 * gb.display_area * .5))
-        x, y = (self.get_x() + self.icon_size[0] / 2, self.get_y() + self.icon_size[1] + (gb.display_height * 0.0125))
+        font = pygame.font.Font("freesansbold.ttf", int(2.2222222222222223e-05 * glob_var.display_area * .5))
+        x, y = (self.get_x() + self.icon_size[0] / 2, self.get_y() + self.icon_size[1] + (glob_var.display_height * 0.0125))
 
         textsurf, textRect = create_textObject(msg1, font)
         textsurf2, textRect2 = create_textObject(msg2, font)
         textRect.center = (x,y)
-        textRect2.center = (x, y + (gb.display_height * 0.01875))
+        textRect2.center = (x, y + (glob_var.display_height * 0.01875))
 
-        self.win.blit(textsurf, textRect)
-        self.win.blit(textsurf2, textRect2)
+        glob_var.win.blit(textsurf, textRect)
+        glob_var.win.blit(textsurf2, textRect2)
 
     def get_damage(self):
         for item in jutsu_signs.names_of_characters:
@@ -340,34 +337,34 @@ class VisualCue:
         self.image_str = image_str
 
         if self.typ == 'header':
-            self.font = pygame.font.Font("freesansbold.ttf", int(0.00014814814814814815 * gb.display_area * .5))
+            self.font = pygame.font.Font("freesansbold.ttf", int(0.00014814814814814815 * glob_var.display_area * .5))
         elif self.typ == 'prompt':
-            self.font = pygame.font.Font("freesansbold.ttf", int(7.407407407407407e-05 * gb.display_area * .5))
+            self.font = pygame.font.Font("freesansbold.ttf", int(7.407407407407407e-05 * glob_var.display_area * .5))
         elif self.typ == 'jutsu':
-            self.font = pygame.font.Font("freesansbold.ttf", int(5.555555555555556e-05 * gb.display_area * .5))
+            self.font = pygame.font.Font("freesansbold.ttf", int(5.555555555555556e-05 * glob_var.display_area * .5))
         else:
-            self.font = pygame.font.Font("freesansbold.ttf", int(9.259259259259259e-05 * gb.display_area * .5))
+            self.font = pygame.font.Font("freesansbold.ttf", int(9.259259259259259e-05 * glob_var.display_area * .5))
 
     def get_x(self):
         if self.typ == 'header':
-            self.x = gb.display_width // 2 - (self.w // 2)
+            self.x = glob_var.display_width // 2 - (self.w // 2)
         elif self.typ == 'prompt':
-            self.x = gb.display_width * ((len(self.seq) + 1) / 6) - (gb.display_width * 0.125)
+            self.x = glob_var.display_width * ((len(self.seq) + 1) / 6) - (glob_var.display_width * 0.125)
         elif self.typ == 'jutsu':
-            self.x = gb.display_width * (len(self.seq) / 6) - (gb.display_width * 0.083333333333)
+            self.x = glob_var.display_width * (len(self.seq) / 6) - (glob_var.display_width * 0.083333333333)
         elif self.typ == 'image':
-            self.x = gb.display_width * (len(self.seq) / 6) - (gb.display_width * 0.1)
+            self.x = glob_var.display_width * (len(self.seq) / 6) - (glob_var.display_width * 0.1)
         return self.x
 
     def get_y(self):
         if self.typ == 'header':
-            self.y = (gb.display_height * 0.025)
+            self.y = (glob_var.display_height * 0.025)
         elif self.typ == 'prompt':
-            self.y = (gb.display_height * 0.1625)
+            self.y = (glob_var.display_height * 0.1625)
         elif self.typ == 'jutsu':
-            self.y = (gb.display_height * 0.3)
+            self.y = (glob_var.display_height * 0.3)
         elif self.typ == 'image':
-            self.y = (gb.display_height * 0.3625)
+            self.y = (glob_var.display_height * 0.3625)
         return self.y
 
     def text_objects(self):
