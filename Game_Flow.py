9dc6d7ce468fa768a2a7a0b0bbf643199c475a10
Avery Griffin <averyzanegriffin@gmail.com>
2020-04-02 10:18:08 -0400

    New layout for game flow.

---
+++ Game_Flow.py
@@ -0,0 +1,39 @@
+import pygame
+
+
+def main_menu():
+    choose_characters()  # when characters are selected and enter is pressed, the character objects are instantiated
+    pass
+
+
+def game_loop():
+    pass
+
+
+def jutsu_loop():
+    pass
+
+
+def end_game():
+    pass
+
+
+def pause_menu():
+    pass
+
+
+# --------------------------
+
+def main_menu2():
+    pass
+
+
+def game_loop2():
+    while game:
+        pass
+    while jutsu:
+        pass
+
+
+def end_game2():
+    pass
