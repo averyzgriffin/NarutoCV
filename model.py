b75569dc9f1f7875739782876b4989071a8e9bf0
Avery Griffin <averyzanegriffin@gmail.com>
2020-04-13 11:58:01 -0400

    This is the best model to date (afaik).

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
 
 
-saved_model = "Machine Learning Content (recording data, training model)/VGG16_EPOCHS10_CODENAME_accumulative_data_4-8_1586391812.960287"
+saved_model = "Machine Learning Content (recording data, training model)/VGG16_EPOCHS50_CODENAME_accumulative_data_4-9_1586483732.9708767"
 # saved_model = "./VGG16_LR_0.0003_EPOCHS1_1571499473.7668839"
